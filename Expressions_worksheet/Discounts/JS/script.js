ca3d9b1d5ed8fd3a809bf91b7128beeb114a6b7f
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 16:57:05 -0400

    Began wiring the code in the js file for the "discounts" problem.

--- Expressions_worksheet/Discounts/JS/script.js
+++ Expressions_worksheet/Discounts/JS/script.js
@@ -5,6 +5,12 @@
  Problem: Discounts
  */
 
+//Calculate the price with tax and without tax of an item that has a discount.
+
+var itemName = prompt("This calculation will give you the price of your item to purchase with sales tax and without sales tax after the discount has been applied.\n\nWhat is the name of item you are purchasing?");  //Prompting the user to enter the name of the item they are purchasing.
+var originalPrice = prompt("What is the original price of the " + itemName + "?\nDo not include the dollar sign, just the price.");   //Prompting the user to enter the original price of their item.
+var discount = prompt("What discount percentage does the " + itemName + " have?\nDo not enter the % sign, just the discount percentage number");   //Prompting the user to enter the percentage amount of discount their item has.
+
 
 
 
