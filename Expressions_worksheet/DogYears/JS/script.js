012e61ba1a82cbafdc7792036b2b745484e71480
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-10 21:08:04 -0400

    More edits to the js file for the dog years problem.

--- Expressions_worksheet/DogYears/JS/script.js
+++ Expressions_worksheet/DogYears/JS/script.js
@@ -6,11 +6,12 @@
  */
 
 // Calculate Sparky the pit bull's age in "dog years" in relation to his age in human years.
-var dogYears = 7; // 1 human year is equivalent to 7 dog years
-var humanYearsAge = prompt("This calculation will help see how old Sparky is in dog years. \n\nHow old is Sparky in human years?");  // Prompting the user to enter how old Sparky is in human years
+var dogYears = 7;   // 1 human year is equivalent to 7 dog years
+var humanYearsAge = prompt("This calculation will help see how old Sparky is in dog years. \n\nHow old is Sparky in human years?");    // Prompting the user to enter how old Sparky is in human years
 
-var result = humanYearsAge * dogYears; // Calculating Sparky's doggy years age
+var result = humanYearsAge * dogYears;  // Calculating Sparky's doggy years age
 
-var result2 = "Sparky is " + humanYearsAge + " human years old, which is " + result + " in dog years."
+var message = "Sparky is " + humanYearsAge + " human years old, which is " + result + " in dog years.";   //Concatenating the resulting message to a string.
 
-console.log(result2);
+console.log(message);   // Display the resulting message in the console.
+alert(message);   // Display the resulting message as an alert to the user.
