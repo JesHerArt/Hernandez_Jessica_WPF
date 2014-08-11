0731bc252c5e8ecaa8e1b3571ba8dfad76aeee93
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 16:22:17 -0400

    Updating the code in the js file for the "average shopping bill" problem.

--- Expressions_worksheet/AverageShoppingBill/JS/script.js
+++ Expressions_worksheet/AverageShoppingBill/JS/script.js
@@ -7,18 +7,18 @@
 
 // REMINDER: ADD IN MY COMMENTS FOR EACH LINE
 
-var grocerySpending = [];
+var grocerySpending = new Array(5);   //Creating the array to hold the grocery spending data for the last 5 weeks.
 
 grocerySpending[0] = prompt("");
-grocerySpending[1] = prompt("Enter the amount spent on groceries in week 2.");
-grocerySpending[2] = prompt("Enter the amount spent on groceries in week 3.");
-grocerySpending[3] = prompt("Enter the amount spent on groceries in week 4.");
-grocerySpending[4] = prompt("Enter the amount spent on groceries in week 5.");
+grocerySpending[1] = prompt("Enter the amount spent on groceries in week 2.");  //Prompting the user to enter the amount of money spent on groceries on week 2.
+grocerySpending[2] = prompt("Enter the amount spent on groceries in week 3.");  //Prompting the user to enter the amount of money spent on groceries on week 3.
+grocerySpending[3] = prompt("Enter the amount spent on groceries in week 4.");  //Prompting the user to enter the amount of money spent on groceries on week 4.
+grocerySpending[4] = prompt("Enter the amount spent on groceries in week 5.");  //Prompting the user to enter the amount of money spent on groceries on week 5.
 
-var total = grocerySpending[0] + grocerySpending[1] + grocerySpending[2] + grocerySpending[3] + grocerySpending[4];
-var average = total / 5;
+var total = grocerySpending[0] + grocerySpending[1] + grocerySpending[2] + grocerySpending[3] + grocerySpending[4];   //Calculating the total grocery spending for the previous five weeks.
+var average = total / 5;   //Calculating the average grocery spending for the last five weeks.
 
-var message = "You have spent a total of $" + total + " on groceries over 5 weeks.\nThat is an average of $" + average + " per week.";
+var message = "You have spent a total of $" + total + " on groceries over 5 weeks.\nThat is an average of $" + average + " per week.";    //Concatenating the resulting message to a string.
 
-console.log(message);
-alert(message);
+console.log(message);   // Display the resulting message in the console.
+alert(message);         // Display the resulting message as an alert to the user.
