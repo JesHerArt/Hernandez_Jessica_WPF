c54ebf1a66f35d4a2c8a6be3e2db202a5623265c
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 16:40:59 -0400

    Added a Math.round() function (in the js file) to the 'average' variable to round to two decimal places for the "average shopping bill" problem.

--- Expressions_worksheet/AverageShoppingBill/JS/script.js
+++ Expressions_worksheet/AverageShoppingBill/JS/script.js
@@ -9,14 +9,14 @@
 
 var grocerySpending = new Array(5);   //Creating the array to hold the grocery spending data for the last 5 weeks.
 
-grocerySpending[0] = prompt("This calculation will help determine what is the total groceries spending amount in the last five weeks and what is the average amount spent per week. \n\nEnter the amount spent on groceries in week 1.");
+grocerySpending[0] = prompt("This calculation will help determine what is the total groceries spending amount in the last five weeks and what is the average amount spent per week. \n\nEnter the amount spent on groceries in week 1.");    //Prompting the user to enter the amount of money spent on groceries on week 1.
 grocerySpending[1] = prompt("Enter the amount spent on groceries in week 2.");  //Prompting the user to enter the amount of money spent on groceries on week 2.
 grocerySpending[2] = prompt("Enter the amount spent on groceries in week 3.");  //Prompting the user to enter the amount of money spent on groceries on week 3.
 grocerySpending[3] = prompt("Enter the amount spent on groceries in week 4.");  //Prompting the user to enter the amount of money spent on groceries on week 4.
 grocerySpending[4] = prompt("Enter the amount spent on groceries in week 5.");  //Prompting the user to enter the amount of money spent on groceries on week 5.
 
 var total = parseFloat(grocerySpending[0]) + parseFloat(grocerySpending[1]) + parseFloat(grocerySpending[2]) + parseFloat(grocerySpending[3]) + parseFloat(grocerySpending[4]);   //Calculating the total grocery spending for the previous five weeks.
-var average = total / 5;   //Calculating the average grocery spending for the last five weeks.
+var average = Math.round( (total / 5) * 100) / 100;   //Calculating the average grocery spending for the last five weeks and rounding to two decimal places.
 
 var message = "You have spent a total of $" + total + " on groceries over 5 weeks.\nThat is an average of $" + average + " per week.";    //Concatenating the resulting message to a string.
 
