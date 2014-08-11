e8c2745a7fc7c8f0ebd66cfa37c0aab93e2cd8ea
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 16:46:57 -0400

    Created the initial folders and files for the "discounts" problem.

---
+++ Expressions_worksheet/Discounts/JS/script.js
@@ -0,0 +1,14 @@
+/* Jessica J. Hernandez
+ ID: 0004631401
+ August 10, 2014
+ Expressions Worksheet Assignment
+ Problem: Discounts
+ */
+
+
+
+
+var message = "";   //Concatenating the resulting message to a string.
+
+console.log(message);   // Display the resulting message in the console.
+alert(message);         // Display the resulting message as an alert to the user.
