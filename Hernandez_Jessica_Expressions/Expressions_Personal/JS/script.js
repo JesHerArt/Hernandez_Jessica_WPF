e53e4300e450ee92f4bd35a40b872c825b6a03b2
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 22:57:29 -0400

    Edited the js file for expressions_personal. Fixed the code for the resulting variables.
    Reminder: Add in comments to each line of code.

--- Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
@@ -5,7 +5,7 @@
  */
 
 //OVERVIEW: Calculate the amount of time it will take to pay off a pending account balance with specified desired minimum payments.
-alert("This program will help calculate your estimated time to pay off your pending balances on your accounts depending on your desired minimum payments you wish to send to send in every month.");  //Prompts the user what this program will do.
+alert("This program will help calculate your estimated time to pay off your pending balances on your accounts depending on your desired minimum payments you wish to send in every month.");  //Prompts the user what this program will do.
 
 
 
@@ -19,16 +19,20 @@ var minPayment = parseFloat( prompt("Enter the minimum payment amount you wish t
 
 
 //RESULT VARIABLES
-var months = accountBalance / minPayment;
+//accountBalance % minPayment == 0
 
-if (months > 0.0)
-   { parseInt(months += 1);}
+//accountBalance / minPayment
+
+var months = parseInt(accountBalance / minPayment);  //Calculate how many months it will take to pay off the pending balance.
+
+if (accountBalance % minPayment > 1)  //Created an if
+   { months += 1;}
 
 
 
 //RESULT TO PRINT
-var message = "";
+var message = accountName + " Payment Calculator\nPending account balance: $" + accountBalance + "\n\nIf you pay a minimum of $" + minPayment + " per month to your " + accountName + " account, you will pay off your pending balance in " + months + " months.";
 
-console.log(message);
+console.log(message);   // Display the resulting message in the console.
 
-alert(message);
+alert(message);         // Display the resulting message as an alert to the user.
