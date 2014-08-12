70a0ba38069a1f2efe873aac23363bf855fb10cb
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-11 22:32:12 -0400

    Added an overview and the givens to the expressions_personal js file.
    Reminder:Continue working on the resulting variables and adding comments to each line of code.

--- Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
@@ -4,3 +4,31 @@
  Expressions Assignment: Personal
  */
 
+//OVERVIEW: Calculate the amount of time it will take to pay off a pending account balance with specified desired minimum payments.
+alert("This program will help calculate your estimated time to pay off your pending balances on your accounts depending on your desired minimum payments you wish to send to send in every month.");  //Prompts the user what this program will do.
+
+
+
+//GIVENS
+var accountName = prompt("Enter the name of the account.");  //Prompts the user to enter the name of their account.
+
+var accountBalance = parseFloat( prompt("Enter the pending balance for the " + accountName + " account.\n** Do not type the '$' sign, just the balance. **") );  //Prompts the user to enter their pending account balance and parses the information.
+
+var minPayment = parseFloat( prompt("Enter the minimum payment amount you wish to send in to your " + accountName + " account every month.\n** Do not type the '$' sign, just the balance. **") );  //Prompts the user to enter their desired minimum payments they want to send in every month.
+
+
+
+//RESULT VARIABLES
+var months = accountBalance / minPayment;
+
+if (months > 0.0)
+   { parseInt(months += 1);}
+
+
+
+//RESULT TO PRINT
+var message = "";
+
+console.log(message);
+
+alert(message);
