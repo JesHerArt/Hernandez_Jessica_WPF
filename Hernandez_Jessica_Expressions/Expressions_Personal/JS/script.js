73e8e01ade6aa3243265bf9aeb202e0b3cdfbb0f
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-12 20:38:03 -0400

    Added additional variables in js file for the expressions_personal code. Included the option to have the user input the time frame they hope to pay off their balance by and it will display a resulting message depending if their pay off months calculation is smaller or bigger than their time frame.
    This should then finalize the code for the expressions_personal portion of the expressions assignments.

--- Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Personal/JS/script.js
@@ -6,7 +6,7 @@
  */
 
 //OVERVIEW: Calculate the amount of time it will take to pay off a pending account balance with specified desired minimum payments.
-alert("This program will help calculate your estimated time to pay off your pending balances on your accounts depending on your desired minimum payments you wish to send in every month.");  //Prompts the user what this program will do.
+alert("This program will help calculate your estimated time to pay off your pending balance on your accounts depending on your desired minimum payments you wish to send in every month.");  //Prompts the user what this program will do.
 
 
 
@@ -17,18 +17,28 @@ var accountBalance = parseFloat( prompt("Enter the pending balance for the " + a
 
 var minPayment = parseFloat( prompt("Enter the minimum payment amount you wish to send in to your " + accountName + " account every month.\n** Do not type the '$' sign, just the balance. **") );  //Prompts the user to enter their desired minimum payments they want to send in every month.
 
+var payOffEstimate = parseInt( prompt("In how many months do you wish/plan to pay off your full balance?") );  //Prompts the user to enter in how many months that would like to pay off their balance.
+
 
 
 //RESULT VARIABLES
 var months = parseInt(accountBalance / minPayment);  //Calculate how many months it will take to pay off the pending balance.
 
 if (accountBalance % minPayment > 1)
-   { months += 1; }                    //Created an if statement because if the remainder of months is greater than 1, then we have to add one more month to months for complete liquidation of the pending balance.
+   { months += 1; }                    //Created an if-statement because if the remainder of months is greater than 1, then we have to add one more month to months for complete liquidation of the pending balance.
+
+var payOffResult;  //This variable will be defined depending on the if-statement that gets executed below.
+
+if ( payOffEstimate > months )
+   { payOffResult = "Therefore, you will be able to pay your balance off sooner the " + payOffEstimate + " months time frame you originally hoped for. Good budgeting!"; }  //This if-statement displays the resulting message if the month calculation is less than the users' estimated pay off month time period.
+
+if ( payOffEstimate < months )
+   { payOffResult = "Unfortunately, you will not be able to pay off your balance in the " + payOffEstimate + " months time frame you originally hoped for. Increasing your minimum monthly payment amount will allow you to pay off your balance sooner.";}  //This if-statement displays the resulting message if the month calculation is greater than the users' estimated pay off month time period.
 
 
 
 //RESULT TO PRINT
-var message = accountName + " Payment Calculator\nPending account balance: $" + accountBalance + "\n\nIf you pay a minimum of $" + minPayment + " per month to your " + accountName + " account, you will pay off your pending balance in " + months + " months.";   //Concatenating the resulting message to a string.
+var message = accountName + " Payment Calculator\nPending account balance: $" + accountBalance + "\n\nIf you pay a minimum of $" + minPayment + " per month to your " + accountName + " account, you will pay off your pending balance in " + months + " months.\n\n" + payOffResult;   //Concatenating the resulting message to a string.
 
 console.log(message);   // Display the resulting message in the console.
 
