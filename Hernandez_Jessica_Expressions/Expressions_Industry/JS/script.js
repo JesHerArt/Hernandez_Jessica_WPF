0c8ef85de180b1284d9358dca2e1c16e4d4c77e8
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-12 23:55:31 -0400

    Began editing code in the js file for "expressions_industry-specific". Declared and defined givens.
    Need to add comments to each line of code.

--- Hernandez_Jessica_Expressions/Expressions_Industry/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Industry/JS/script.js
@@ -5,3 +5,33 @@
  Expressions Assignment: Industry-specific
  */
 
+//OVERVIEW:
+
+
+
+//GIVENS
+var price = parseFloat( prompt("How much did you charge your client?") );
+
+var pages = parseInt( prompt("How many pages does the website you created for your client have?") );
+
+var pagesData = new Array();
+
+var pageNum = 0;
+
+for ( k = 0; k < pages; k++ )
+    {
+        pagesData = parseInt( prompt("How many kb is page #" + pageNum++ ) );
+    }
+
+
+
+//RESULT VARIABLES
+
+
+
+//RESULT TO PRINT
+var message = "";  //Concatenating the resulting message to a string.
+
+console.log(message);   // Display the resulting message in the console.
+
+alert(message);         // Display the resulting message as an alert to the user.
