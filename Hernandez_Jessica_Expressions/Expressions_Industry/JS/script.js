bbf93ed079e76abe782a85b5a20159c785005487
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-13 00:30:24 -0400

    Added comments to the givens and constants sections in the js file for "expressions-industry-specific"

--- Hernandez_Jessica_Expressions/Expressions_Industry/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Industry/JS/script.js
@@ -10,18 +10,21 @@
 
 
 //GIVENS
-var price = parseFloat( prompt("How much did you charge your client?") );
+var price = parseFloat( prompt("How much did you charge your client?\n** Do not enter the '$' sign, just the price. **") );  //Prompts the user to enter the price they charged the client to do their website.
 
-var pages = parseInt( prompt("How many pages does the website you created for your client have?") );
+var pages = parseInt( prompt("How many pages does the website you created for your client have?") );  //Prompts the user to input how many pages they created for the clients website.
 
-var pagesData = new Array();
+var pagesData = new Array();  //Declare the array needed to store the information for the givens the user will be prompted to input.
 
-for ( k = 0; k < pages; k++ )
+for ( k = 0; k < pages; k++ )  //This for-loop will define the the pagesData array by prompting the user to enter how much kb is each page of the website depending on how many web pages the user previously said they had created.
     {
-        var pageNum = 0;
+        var pageNum = 0;  //Variable created in the for-loop for the sake of displaying page #'s in the prompt.
         pagesData[k] = parseInt( prompt("How many kb is page #" + pageNum++ ) );
     }
 
+
+
+//CONSTANTS
 var DSL_STANDARD_KBPS = 3000;  //Constant variable for the standard DSL download speed in kb per second.
 
 var DIAL_UP_STANDARD_KBPS = 56;  //Constant variable for the standard Dial-up download speed in kb per second.
