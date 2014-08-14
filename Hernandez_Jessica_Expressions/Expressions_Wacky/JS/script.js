0e678c34a9dc62055bccb89deb8996592e84c6f9
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-13 23:27:53 -0400

    "expressions_wacky": Added/declared all the variables that will be used in the code.

--- Hernandez_Jessica_Expressions/Expressions_Wacky/JS/script.js
+++ Hernandez_Jessica_Expressions/Expressions_Wacky/JS/script.js
@@ -14,7 +14,45 @@ var placeOrderResult;
 
 var message;
 
-var placeOrder = parseInt( prompt( "Thank you for visiting Pizza Script!\nAt the moment our site is experiencing some technical difficulties, BUT we will still be able to take your order. In order to do so, you will be prompted with a series of dialog boxes requesting your specified input for your order.\n\nWe cannot accept credit cards through this interface, but we will be able to give you a full order price estimate before you actually submit your order. This will allow you time to verify if you have the sufficient funds in cash to therefore place your order.\n\n\nWould you like to place an order?\n\n1 - Yes\n\n2 - No\n\n\n" + inputMessage ) );
+var pizza;
+
+var pizzaSize;
+
+var pizzaSizePrice;
+
+var toppingName;
+
+var toppingPrice;
+
+var beverageName;
+
+var beveragePrice;
+
+var dessertName;
+
+var dessertPrice;
+
+var customerName;
+
+var phoneNum;
+
+var streetAddress;
+
+var cityStateZip;
+
+var tipAmount;
+
+var deliveryPrice;
+
+var subTotal;
+
+var orderReview;
+
+var submitOrder;
+
+var orderTotal;
+
+var placeOrder = parseInt( prompt( "Thank you for visiting Pizza Script!\nAt the moment our site is experiencing some technical difficulties, BUT we will still be able to take your order. In order to do so, you will be prompted with a series of dialog boxes requesting your specified input for your order.\n\nWe cannot accept credit cards through this interface, but we will be able to give you a full order price estimate before you actually submit your order. This will allow you time to verify if you have the sufficient funds in cash to therefore place your order.\n\n\nWould you like to place an order?\n\n1 - Yes\n2 - No\n\n" + inputMessage ) );
 
 if ( placeOrder == 1 )
     {
@@ -22,13 +60,15 @@ if ( placeOrder == 1 )
     }
 
 if ( placeOrder == 2 )
-{
-    placeOrderResult = false;
-    message = "Thank you for visiting Pizza Script; sorry our site wasn't fully functional at the moment.\nHave a nice day!";
-}
+    {
+        placeOrderResult = false;
+        message = "Thank you for visiting Pizza Script; sorry our site wasn't completely functional at the moment.\n\nHave a nice day!";
+    }
 
 while (placeOrderResult)
     {
+        var steps = 1;
+
 
 
 
@@ -45,7 +85,7 @@ while (placeOrderResult)
 
 
 //RESULT TO PRINT
-var orderReview;
+//var orderReview;
 
 //var message;  //Concatenating the resulting message to a string.
 
