072c99775eef6387cf5e7112318492d16a0aae24
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-18 13:45:28 -0400

    Conditionals Worksheet: Created the initial necessary folders and files for the assignment.

---
+++ Conditionals_worksheet/Group_3/JS/script.js
@@ -0,0 +1,26 @@
+/* Jessica J. Hernandez
+ ID: 0004631401
+ August 18, 2014
+ Conditionals Worksheet Assignment
+ Group 3:
+ */
+
+//OVERVIEW:
+
+
+
+//GIVENS
+
+
+
+//RESULT VARIABLES
+
+
+
+
+//RESULT TO PRINT
+var message = "";   //Concatenating the resulting message to a string.
+
+console.log(message);   // Display the resulting message in the console.
+
+alert(message);         // Display the resulting message as an alert to the user.
