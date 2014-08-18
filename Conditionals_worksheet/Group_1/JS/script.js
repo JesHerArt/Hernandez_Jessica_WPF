62a3d7fc5be40d9893bc57ecb6380a7a05402bd8
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-18 15:01:34 -0400

    Conditionals worksheet (group 1): Finalized the celsius/fahrenheit converter. All formulas work. All comments are written. The messages display correctly.

--- Conditionals_worksheet/Group_1/JS/script.js
+++ Conditionals_worksheet/Group_1/JS/script.js
@@ -6,26 +6,27 @@
  */
 
 //OVERVIEW:
+alert( "TEMPERATURE CONVERSION\nThe following prompts will ask for your input to then determine the temperature conversion changes for you in either Celsius or Fahrenheit." );  //Prompts the user what the following program/script is running.
 
 
 
 //GIVENS
-var degrees = parseInt( prompt( "How many degrees is there outside?" ) );
-var unit = prompt( "What degree unit would you like to convert to?\n\nPlease enter:\nF - To convert " + degrees + "\xB0 Celsius to Fahrenheit.\nor\nC - To convert " + degrees + "\xB0 Fahrenheit to Celsius." );
+var degrees = Number( prompt( "How many degrees is there outside?" ) );  //Prompts the user to enter how many degrees it is outside. The input gets parsed to a number.
+var unit = prompt( "What degree unit would you like to convert to?\n\nPlease enter:\nF - To convert " + degrees + "\xB0 Celsius to Fahrenheit.\nor\nC - To convert " + degrees + "\xB0 Fahrenheit to Celsius." );  //Declare and define the variable with the
 
 
 
 //RESULT VARIABLES
-var conversion;
-var message;
+var conversion;  //Declare the conversion variable to use to store the result of the conversion being done.
+var message;  //Declare the message variable to use to store the concatenated resulting message.
 
-if ( unit === "F" || unit === "f" ){
-    conversion = 9/5 * (degrees + 32);
+if ( unit === "F" || unit === "f" ){  //This if determines if the unit measurement entered is equivalent to F or f.
+    conversion = Math.round( ( 9/5 * degrees + 32 ) * 100 ) / 100;  //The conversion variable gets defined to the result of the celsius to fahrenheit conversion.
 
     message = "The temperature " + degrees + "\xB0 Celsius is equivalent to " + conversion + "\xB0 Fahrenheit.";  //Concatenating the resulting message to a string.
 
-}else if ( unit === "C" || unit === "c"){
-    conversion = 5/9 * (degrees - 32);
+}else if ( unit === "C" || unit === "c"){  //This else if determines if the unit measurement entered is equivalent to C or c.
+    conversion = Math.round( ( 5/9 * (degrees - 32) ) * 100 ) / 100;  //The conversion variable gets defined to the result of the fahrenheit to celsius conversion.
 
     message = "The temperature " + degrees + "\xB0 Fahrenheit is equivalent to " + conversion + "\xB0 Celsius.";  //Concatenating the resulting message to a string.
 
