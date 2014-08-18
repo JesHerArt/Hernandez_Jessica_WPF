02fd986b80e16dd54796f986d4c51f016d692334
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-18 16:25:19 -0400

    Conditionals worksheet (group 3): Began wiring code in the JS file. Declared the array and some of the resulting variables. Need to add comments to each line and create the conditional statements.

--- Conditionals_worksheet/Group_3/JS/script.js
+++ Conditionals_worksheet/Group_3/JS/script.js
@@ -2,18 +2,29 @@
  ID: 0004631401
  August 18, 2014
  Conditionals Worksheet Assignment
- Group 3:
+ Group 3: Tire Pressure 1
  */
 
 //OVERVIEW:
+alert( "TIRE PRESSURE SPEC CHECK\nThe following prompts will ask you to enter the tire pressure (in PSI) of each tire to then determine if your tires pass spec or if they need to be checked out." );  //Prompts the user what the following program/script is running.
 
 
 
 //GIVENS
+var tirePressure = new Array(4);
+
+tirePressure[0] = parseInt( prompt( "Please input the tire pressure (in PSI) of the first front (driver's side) tire." ) );
+
+tirePressure[1] = parseInt( prompt( "Please input the tire pressure (in PSI) of the second front (passenger's side) tire." ) );
+
+tirePressure[2] = parseInt( prompt( "Please input the tire pressure (in PSI) of the first back (driver's side) tire." ) );
+
+tirePressure[3] = parseInt( prompt( "Please input the tire pressure (in PSI) of the second back (passenger's side) tire." ) );
 
 
 
 //RESULT VARIABLES
+var specCheckResult;
 
 
 
