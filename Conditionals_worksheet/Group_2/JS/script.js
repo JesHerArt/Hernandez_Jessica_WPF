af69b1aa43c0c2a8f69a1de837564948aa72dc75
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-18 15:46:17 -0400

    Conditionals worksheet (group 2): Began writing code the grade letter calculator. WebStorm is giving a couple of errors. Need to be fixed. Comments need to be added in to each line.

--- Conditionals_worksheet/Group_2/JS/script.js
+++ Conditionals_worksheet/Group_2/JS/script.js
@@ -2,7 +2,7 @@
  ID: 0004631401
  August 18, 2014
  Conditionals Worksheet Assignment
- Group 2:
+ Group 2: Grade Letter
  */
 
 //OVERVIEW:
@@ -10,17 +10,60 @@
 
 
 //GIVENS
-
+var gradePercent = parseInt( prompt( "What was the final grade percentage you received in your course?" ) );
 
 
 //RESULT VARIABLES
+var grade;
+var message;
 
+if ( gradePercent >= 95 && gradePercent <= 100 ){
+    grade = "A+";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class!";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 90 && gradePercent <= 94 ){
+    grade = "A";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class!";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 85 && gradePercent <= 89 ){
+    grade = "B+";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class!";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 80 && gradePercent <= 84 ) {
+    grade = "B";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class!";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 76 && gradePercent <= 79 ) {
+    grade = "C+";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class.";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 73 && gradePercent <= 75 ) {
+    grade = "C";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class.";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 70 && gradePercent <= 72 ) {
+    grade = "D";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class.";  //Concatenating the resulting message to a string.
+
+}else if ( gradePercent >= 0 && gradePercent <= 69 ) {
+    grade = "F";
+
+    message = "You have " + gradePercent + "%, which means you have earned a(n) " + grade + " in the class.";  //Concatenating the resulting message to a string.
+
+}else ( gradePercent <= -1 || gradePercent >= 101 ) {
+    message = "You have entered an invalid grade percentage. Please try again.";  //Concatenating the resulting message to a string.
+}
 
 
 
 //RESULT TO PRINT
-var message = "";   //Concatenating the resulting message to a string.
-
 console.log(message);   // Display the resulting message in the console.
 
 alert(message);         // Display the resulting message as an alert to the user.
