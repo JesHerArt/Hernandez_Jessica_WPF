f5b2be928eac486106183584c8091ad1328e7336
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-18 14:44:36 -0400

    Conditionals Worksheet (group 1): Began adding code to the JS file. Need to add comments. Units are reading correctly. Conversion formula needs to be checked.

--- Conditionals_worksheet/Group_1/JS/script.js
+++ Conditionals_worksheet/Group_1/JS/script.js
@@ -2,7 +2,7 @@
  ID: 0004631401
  August 18, 2014
  Conditionals Worksheet Assignment
- Group 1:
+ Group 1: Celsius to Fahrenheit Converter
  */
 
 //OVERVIEW:
@@ -10,17 +10,32 @@
 
 
 //GIVENS
+var degrees = parseInt( prompt( "How many degrees is there outside?" ) );
+var unit = prompt( "What degree unit would you like to convert to?\n\nPlease enter:\nF - To convert " + degrees + "\xB0 Celsius to Fahrenheit.\nor\nC - To convert " + degrees + "\xB0 Fahrenheit to Celsius." );
 
 
 
 //RESULT VARIABLES
+var conversion;
+var message;
 
+if ( unit === "F" || unit === "f" ){
+    conversion = 9/5 * (degrees + 32);
+
+    message = "The temperature " + degrees + "\xB0 Celsius is equivalent to " + conversion + "\xB0 Fahrenheit.";  //Concatenating the resulting message to a string.
+
+}else if ( unit === "C" || unit === "c"){
+    conversion = 5/9 * (degrees - 32);
+
+    message = "The temperature " + degrees + "\xB0 Fahrenheit is equivalent to " + conversion + "\xB0 Celsius.";  //Concatenating the resulting message to a string.
+
+}else {
+    message = "You have entered an invalid unit measurement. Try again.";  //Concatenating the resulting message to a string.
+}
 
 
 
 //RESULT TO PRINT
-var message = "";   //Concatenating the resulting message to a string.
-
 console.log(message);   // Display the resulting message in the console.
 
 alert(message);         // Display the resulting message as an alert to the user.
