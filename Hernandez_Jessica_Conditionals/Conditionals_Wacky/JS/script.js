7af9ab3ba5155dcf0e523736fb2bd163f8e1c8a3
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-20 19:29:16 -0400

    Conditionals Assignment (wacky): Concatenated the resulting strings for the message to be displayed depending on the condition if the monthly total cost is below the budget or meets the budget requirement.   Code can now be tested and makes any troubleshooting adjustments if necessary.

--- Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
@@ -7,7 +7,11 @@
  */
 
 //OVERVIEW:
-alert( "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you want to buy a new car. The special financing deal at the moment is that you can buy any vehicle and pay a flat monthly payment of $295.00 a month, regardless of which model you buy (this includes the luxury class vehicles as well.\n\nAlthough the monthly finance payment is $295.00 per month, your car insurance company's monthly premium would be based on the total value of the vehicle.\n\nYou want to find out if your monthly car budget can cover the car expenses of owning a new vehicle along with the car insurance." );
+var overview = "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you want to buy a new car. The special financing deal at the moment is that you can buy any vehicle and pay a flat monthly payment of $295.00 a month, regardless of which model you buy (this includes the luxury class vehicles as well.\n\nAlthough the monthly finance payment is $295.00 per month, your car insurance company's monthly premium would be based on the total value of the vehicle.\n\nYou want to find out if your monthly car budget can cover the car expenses of owning a new vehicle along with the car insurance.";
+
+console.log (overview);
+
+alert(overview);
 
 
 
@@ -77,3 +81,22 @@ if ( carValue < value1 ){
 
 var monthlyTotalCarCost = carFinance + insuranceCost;
 var payOffTime = carValue / carFinance;
+
+
+
+var message;
+if ( monthlyTotalCarCost > budget ){
+
+    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nSorry, but at this moment your monthly car expenses budget cannot cover the total monthly car cost of $" + monthlyTotalCarCost.toFixed(2) + ", which includes the vehicle financing payments of $295.00 plus the tier " + tier + " cost of $" + insuranceCost.toFixed(2) + " of insurance payments.";  //Concatenating the resulting message to a string.
+
+}else {
+
+    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nCongratulations! You will be able to afford a new car and the corresponding monthly car insurance payments according to your estimated monthly car expenses budget of $" + budget.toFixed(2) + ".\n\nCar Value: $" + carValue + "\nCar Finance: $" + carFinance.toFixed(2) + "\nInsurance Cost (Tier " + tier + "): $" + insuranceCost + "\n\nTotal Monthly Car Expenses: $" + monthlyTotalCarCost + "\n\nYou will be able to pay off your car in full in " + payOffTime + " months, if you send in the minimum car financing payment in each month.";  //Concatenating the resulting message to a string.
+
+}
+
+
+
+console.log(message);   // Display the resulting message in the console.
+
+alert(message);         // Display the resulting message as an alert to the user.
