aae9a79efc895415c1f669aa6b2db9ebbb89965a
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-20 19:12:42 -0400

    Conditionals Assignment (wacky): Defined the given variables and added validation checks to each prompt. Created the conditional statements for the insurance cost based on the total car value. Declared and defined the monthlyTotalCarCost and payOffTime variables to be used in the calculation.

--- Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
@@ -14,6 +14,7 @@ alert( "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you wan
 //CONSTANTS:
 var carFinance = 295.00;
 
+var value0 = 0;
 var value1 = 20000;
 var value2 = 40000;
 var value3 = 60000;
@@ -28,5 +29,51 @@ var tier5 = 525.00;
 
 
 //GIVENS:
-var budget;
-var carValue;
+var budget = parseFloat( prompt( "What is your monthly budget for use on car related monthly payments?" ) );
+
+if ( budget === "" ){  //Validation
+    budget = parseFloat( prompt( "You've input an invalid Budget amount, please try again.\n\nWhat is your monthly budget for use on car related monthly payments?" ) );
+}
+
+var carValue = parseFloat( prompt( "What is the total value of the vehicle you want to finance?" ) );
+
+if ( carValue === "" || carValue < value0 ){  //Validation
+    carValue = parseFloat( prompt( "You've input an invalid Car Value amount, please try again.\n\nWhat is the total value of the vehicle you want to finance?" ) );
+}
+
+
+
+var tier;
+var insuranceCost;
+
+if ( carValue < value1 ){
+
+    insuranceCost = tier1;
+    tier = 1;
+
+}else if ( carValue > value1 && carValue < value2 ){
+
+    insuranceCost = tier2;
+    tier = 2;
+
+}else if ( carValue > value2 && carValue < value3 ){
+
+    insuranceCost = tier3;
+    tier = 3;
+
+}else if ( carValue > value3 && carValue < value4 ){
+
+    insuranceCost = tier4;
+    tier = 4;
+
+}else {
+
+    insuranceCost = tier5;
+    tier = 5;
+
+}
+
+
+
+var monthlyTotalCarCost = carFinance + insuranceCost;
+var payOffTime = carValue / carFinance;
