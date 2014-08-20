80cc08cff351ec04910337f85bf70de240447ea1
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-20 18:56:14 -0400

    Conditionals assignment (wacky): Created the overview for the user to understand what the program will be calculating. Created the constant variables for the car financing price, car values and insurance cost tiers. Declared the two given variables.

--- Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
@@ -6,3 +6,27 @@
  Conditionals Assignment: Wacky
  */
 
+//OVERVIEW:
+alert( "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you want to buy a new car. The special financing deal at the moment is that you can buy any vehicle and pay a flat monthly payment of $295.00 a month, regardless of which model you buy (this includes the luxury class vehicles as well.\n\nAlthough the monthly finance payment is $295.00 per month, your car insurance company's monthly premium would be based on the total value of the vehicle.\n\nYou want to find out if your monthly car budget can cover the car expenses of owning a new vehicle along with the car insurance." );
+
+
+
+//CONSTANTS:
+var carFinance = 295.00;
+
+var value1 = 20000;
+var value2 = 40000;
+var value3 = 60000;
+var value4 = 80000;
+
+var tier1 = 150.00;
+var tier2 = 225.00;
+var tier3 = 300.00;
+var tier4 = 415.00;
+var tier5 = 525.00;
+
+
+
+//GIVENS:
+var budget;
+var carValue;
