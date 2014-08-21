e66e7799ffefaf139494df52aec2944484136862
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-20 23:35:40 -0400

    Conditionals Assignment (industry): The code has been fully troubleshooted. The output messages are correctly being displayed. The calculation works thoroughly. All the comments for each line of code has been written in. Industry conditionals part of the assignment is complete.

--- Hernandez_Jessica_Conditionals/Conditionals_Industry/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Industry/JS/script.js
@@ -14,63 +14,63 @@ alert(overview);  // Display the overview message as an alert to the user.
 
 
 //CONSTANTS:
-var oneFoot = 12;
+var oneFoot = 12;  //12 inches are in 1 foot.
 
-var shop2FlatRate = 5.00;
+var shop2FlatRate = 5.00;  //Flat rate fee for Quick Print
 
-var shop2SqFtRate = 1.25;
+var shop2SqFtRate = 1.25;  //Per square foot rate in addition to the flat rate fee at Quick Print.
 
 
 
 //GIVENS:
-var posterWidth = prompt( "Enter the poster's width (in inches)." );
+var posterWidth = prompt( "Enter the poster's width (in inches)." );  //Prompting the user to enter the poster width in inches.
 
 if ( posterWidth === "" || posterWidth < 0){  //Poster width input validation.
 
-    posterWidth = prompt( "You've input an invalid Poster Width size, please try again.\n\n\nEnter the poster's width (in inches)." );
+    posterWidth = prompt( "You've input an invalid Poster Width size, please try again.\n\n\nEnter the poster's width (in inches)." );  //Prompting the user an invalid input for the poster width was made. And prompting the user again to enter the poster width in inches.
 
 }
 
-posterWidth = Number(posterWidth);
+posterWidth = Number(posterWidth);  //Parsing the poster width input as a number variable.
 
 
-var posterLength = prompt( "Enter the poster's length (in inches)." );
+var posterLength = prompt( "Enter the poster's length (in inches)." );  //Prompting the user to enter the poster length in inches.
 
 if ( posterLength === "" || posterLength < 0){  //Poster length input validation.
 
-    posterLength = prompt( "You've input an invalid Poster Length size, please try again.\n\n\nEnter the poster's length (in inches)." );
+    posterLength = prompt( "You've input an invalid Poster Length size, please try again.\n\n\nEnter the poster's length (in inches)." );  //Prompting the user an invalid input for the poster length was made. And prompting the user again to enter the poster length in inches.
 
 }
 
-posterLength = Number(posterLength);
+posterLength = Number(posterLength);  //Parsing the poster length input as a number variable.
 
 
-var shop1SqFtRate = prompt( "What is the current deal for poster print jobs at PrintingPlus (print shop #1) this week?\nWeekly deals range between $1.50 - $2.75 per square foot.\n\n** Do not input the '$' symbol, just the amount. **" );
+var shop1SqFtRate = prompt( "What is the current deal for poster print jobs at PrintingPlus (print shop #1) this week?\nWeekly deals range between $1.50 - $2.75 per square foot.\n\n** Do not input the '$' symbol, just the amount. **" );  //Prompting the sure to enter the weekly deal price at PrintingPlus.
 
-if ( shop1SqFtRate === "" || shop1SqFtRate < 0){  //Poster length input validation.
+if ( shop1SqFtRate === "" || shop1SqFtRate < 0){  //Poster rate input validation.
 
-    shop1SqFtRate = prompt( "You've input an invalid Rate for the poster printing deal pricing, please try again.\n\n\nWhat is the current deal for poster print jobs at PrintingPlus (print shop #1) this week?\nWeekly deals range between $1.50 - $2.75 per square foot.\n\n** Do not input the '$' symbol, just the amount. **" );
+    shop1SqFtRate = prompt( "You've input an invalid Rate for the poster printing deal pricing, please try again.\n\n\nWhat is the current deal for poster print jobs at PrintingPlus (print shop #1) this week?\nWeekly deals range between $1.50 - $2.75 per square foot.\n\n** Do not input the '$' symbol, just the amount. **" );  //Prompting the user an invalid input for the poster rate at PrintingPlus was made. And prompting the user again to enter the weekly deal price at PrintingPlus.
 
 }
 
-shop1SqFtRate = parseFloat( shop1SqFtRate );
+shop1SqFtRate = parseFloat( shop1SqFtRate );  //Parsing the shop1SqFtRate input as a double variable.
 
 
 
 //RESULT VARIABLES:
-var posterSqFt = (posterLength + posterWidth) / oneFoot;
+var posterSqFt = (posterLength + posterWidth) / oneFoot;  //Declaring and defining the variable to store the total poster square footage.
 
-var shop1Cost = posterSqFt * shop1SqFtRate;
+var shop1Cost = posterSqFt * shop1SqFtRate;  //Declaring and defining the variable to store the total printing cost at PrintingPlus.
 
-var shop2Cost = posterSqFt * shop2SqFtRate + shop2FlatRate;
+var shop2Cost = posterSqFt * shop2SqFtRate + shop2FlatRate;  //Declaring and defining the variable to store the total printing cost at Quick Print.
 
-var costDifference;
+var costDifference;  //Declaring the variable. This will hold a concatenated message.
 
 costDifference = ( shop1Cost < shop2Cost ) ? "PrintingPlus (Print Shop #1) would cost you less to print a " + posterWidth + "in. x " + posterLength + "in. color poster.\n\nWith rate of $" + shop1SqFtRate.toFixed(2) + " per Sq. Ft., your total spending cost is $" + shop1Cost.toFixed(2) + ".\n\nYou will save $" + (shop2Cost - shop1Cost).toFixed(2) + " by printing the poster at PrintingPlus." : "Quick Print (Print Shop #2) would cost you less to print a " + posterWidth + "in. x " + posterLength + "in. color poster.\n\nWith rate of $" + shop2SqFtRate.toFixed(2) + " per Sq. Ft. plus the flat rate fee of $" + shop2FlatRate.toFixed(2) + ", your total spending cost is $" + shop2Cost.toFixed(2) + ".\n\nYou will save $" + (shop1Cost - shop2Cost).toFixed(2) + " by printing the poster at Quick Print.";  //Concatenating the resulting message to a string.
 
-if ( shop1Cost === shop2Cost ){
+if ( shop1Cost === shop2Cost ){  //Although the ternary statement checks to see which cost is less, this if statement checks to see if both costs equal the same amount.
 
-    costDifference = "Both PrintingPlus (Print Shop #1) and Quick Print (Print Shop #2) have the same printing cost to print a " + posterWidth + "in. x " + posterLength + "in. color poster this week. Visit which ever print shop is closest to you at the moment.\n\nYour total spending cost is $" + shop1Cost + ".";
+    costDifference = "Both PrintingPlus (Print Shop #1) and Quick Print (Print Shop #2) have the same printing cost to print a " + posterWidth + "in. x " + posterLength + "in. color poster this week. Visit which ever print shop is closest to you at the moment.\n\nYour total spending cost is $" + shop1Cost.toFixed(2) + ".";  //Therefore, if both cost are the same amount, the costDifference variable gets replaced with this concatenated message string instead.
 
 }
 
