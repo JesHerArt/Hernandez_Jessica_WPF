f8dd0f28620e0e078bf6e5a16ca76423e0096ded
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-20 20:44:59 -0400

    Conditionals Assignment (wacky): The code has been troubleshooted. The output message spacing has been aligned. The calculation now works thoroughly.
    To do: Add comments to each line of code.

--- Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Wacky/JS/script.js
@@ -7,9 +7,7 @@
  */
 
 //OVERVIEW:
-var overview = "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you want to buy a new car. The special financing deal at the moment is that you can buy any vehicle and pay a flat monthly payment of $295.00 a month, regardless of which model you buy (this includes the luxury class vehicles as well.\n\nAlthough the monthly finance payment is $295.00 per month, your car insurance company's monthly premium would be based on the total value of the vehicle.\n\nYou want to find out if your monthly car budget can cover the car expenses of owning a new vehicle along with the car insurance.";
-
-console.log (overview);
+var overview = "MONTHLY CAR EXPENSES CALCULATOR\n\nYou are at the car dealer and you want to buy a new car. The special financing deal at the moment is that you can buy any vehicle and pay a flat monthly payment of $295.00 a month, regardless of which model you buy (this includes the luxury class vehicles as well).\n\nAlthough the monthly finance payment is $295.00 per month, your car insurance company's monthly premium would be based on the total value of the vehicle.\n\nYou want to find out if your monthly car budget can cover the car expenses of owning a new vehicle along with the corresponding car insurance payments.";
 
 alert(overview);
 
@@ -33,21 +31,32 @@ var tier5 = 525.00;
 
 
 //GIVENS:
-var budget = parseFloat( prompt( "What is your monthly budget for use on car related monthly payments?" ) );
+var budget = prompt( "What is your monthly budget for use on car related monthly payments?\n\n** Do not put the '$' symbol, just the amount. Ex: 350.00. **" );
 
 if ( budget === "" ){  //Validation
-    budget = parseFloat( prompt( "You've input an invalid Budget amount, please try again.\n\nWhat is your monthly budget for use on car related monthly payments?" ) );
+
+    budget = prompt( "You've input an invalid Budget amount, please try again.\n\nWhat is your monthly budget for use on car related monthly payments?\n\n** Do not put the '$' symbol, just the amount. Ex: 350.00. **" );
+
 }
 
-var carValue = parseFloat( prompt( "What is the total value of the vehicle you want to finance?" ) );
+budget = parseFloat( budget );
 
-if ( carValue === "" || carValue < value0 ){  //Validation
-    carValue = parseFloat( prompt( "You've input an invalid Car Value amount, please try again.\n\nWhat is the total value of the vehicle you want to finance?" ) );
+
+var carValue = prompt( "What is the total value of the vehicle you want to finance?\n\n** Do not put the '$' symbol, just the amount. Ex: 18000.00. **" );
+
+if ( carValue == "" || carValue < value0 ){  //Validation
+
+    carValue = prompt( "You've input an invalid Car Value amount, please try again.\n\nWhat is the total value of the vehicle you want to finance?\n\n** Do not put the '$' symbol, just the amount. Ex: 18000.00. **" );
+
 }
 
+carValue = parseFloat( carValue );
 
 
+
+//RESULT VARIABLES:
 var tier;
+
 var insuranceCost;
 
 if ( carValue < value1 ){
@@ -77,25 +86,29 @@ if ( carValue < value1 ){
 
 }
 
-
-
 var monthlyTotalCarCost = carFinance + insuranceCost;
-var payOffTime = carValue / carFinance;
 
+var payOffTime = parseInt( Math.round ( (carValue / carFinance) * 100 ) / 100 );
 
+if ( carValue % carFinance > 0 ){
 
-var message;
-if ( monthlyTotalCarCost > budget ){
-
-    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nSorry, but at this moment your monthly car expenses budget cannot cover the total monthly car cost of $" + monthlyTotalCarCost.toFixed(2) + ", which includes the vehicle financing payments of $295.00 plus the tier " + tier + " cost of $" + insuranceCost.toFixed(2) + " of insurance payments.";  //Concatenating the resulting message to a string.
-
-}else {
-
-    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nCongratulations! You will be able to afford a new car and the corresponding monthly car insurance payments according to your estimated monthly car expenses budget of $" + budget.toFixed(2) + ".\n\nCar Value: $" + carValue + "\nCar Finance: $" + carFinance.toFixed(2) + "\nInsurance Cost (Tier " + tier + "): $" + insuranceCost + "\n\nTotal Monthly Car Expenses: $" + monthlyTotalCarCost + "\n\nYou will be able to pay off your car in full in " + payOffTime + " months, if you send in the minimum car financing payment in each month.";  //Concatenating the resulting message to a string.
+    payOffTime += 1;
 
 }
 
 
+
+//RESULT TO PRINT:
+var message;
+if ( monthlyTotalCarCost > budget ){
+
+    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nSorry, but at this moment your monthly car expenses budget ($" + budget.toFixed(2) + ") cannot cover the total monthly car expenses cost of $" + monthlyTotalCarCost.toFixed(2) + ", which includes the vehicle financing payments of $295.00 plus the tier " + tier + " insurance payment cost of $" + insuranceCost.toFixed(2) + ".";  //Concatenating the resulting message to a string.
+
+}else {
+
+    message = "MONTHLY CAR EXPENSES CALCULATOR\n\nCongratulations! You will be able to afford a new car and the corresponding monthly car insurance payments according to your estimated monthly car expenses budget.\n\nBudget ..................................... $" + budget.toFixed(2) + "\nCar Value ................................. $" + carValue.toFixed(2) + "\nCar Finance .............................. $" + carFinance.toFixed(2) + "\nInsurance Cost (Tier " + tier + ") ............. $" + insuranceCost.toFixed(2) + "\n\nTotal Monthly Car Expenses ...... $" + monthlyTotalCarCost.toFixed(2) + "\n\nif you send in the minimum car financing payment each month, you will be able to pay off your car in full in " + payOffTime + " months.";  //Concatenating the resulting message to a string.
+
+}
 
 console.log(message);   // Display the resulting message in the console.
 
