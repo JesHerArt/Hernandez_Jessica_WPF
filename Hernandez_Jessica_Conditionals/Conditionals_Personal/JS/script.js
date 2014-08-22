3eebcfab649d313e52b3d33c1b1a22775bc0dd98
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-21 20:59:57 -0400

    Conditionals Assignment (personal): All the constants needed to be used for this calculation have been declared and defined. The comment for each line of constants code has been added as well.

--- Hernandez_Jessica_Conditionals/Conditionals_Personal/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Personal/JS/script.js
@@ -14,3 +14,18 @@ alert(overview);  // Display the overview message as an alert to the user.
 
 
 //CONSTANTS:
+var oilChangeMileage = 3000;  //Constant for: Oil change to be done every 3,000 miles.
+
+var rotateTiresMileage = 5000;  //Constant for: Tires to rotate every 5,000 miles.
+
+var brakesMileage = 10000;  //Constant for: Brakes to be replaced every 10,000 miles.
+
+var sparkPlugMileage = 20000;  //Constant for: Spark Plugs to be replaced every 20,000 miles.
+
+var motorMountsMileage = 100000;  //Constant for: Motor Mounts to be replaced at 100,000 miles.
+
+var serviceCharge = 50.00;  //Service charge of $50.00 as part of the maintenance service quote to give the customer.
+
+
+
+//GIVENS:
