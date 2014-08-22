8b05e04d46fc46a109475d0841ed076b38709d94
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-21 20:53:19 -0400

    Conditionals Assignment (personal): Created the overview section explaining to the user what the calculation does. Depending on the previous car milage and the current car milage, is what required maintenance is needed for the vehicle and an estimate of the services.

--- Hernandez_Jessica_Conditionals/Conditionals_Personal/JS/script.js
+++ Hernandez_Jessica_Conditionals/Conditionals_Personal/JS/script.js
@@ -6,3 +6,11 @@
  Conditionals Assignment: Personal
  */
 
+//OVERVIEW:
+var overview = "OIL CHANGE & MAINTENANCE CHECK\n\nYou have received a recall notice in the mail for your car. Anything having to be done to the car for the recall will be covered by the dealer. But while you're there you ask to get a multi-point inspection regardless, to see what additional maintenance requirements need to be done since the last time you had a previous tune-up. You will then be provided with an estimate as to what maintenance is required for your vehicle at the moment.";  //Message to display to user about the overview of this calculation.
+
+alert(overview);  // Display the overview message as an alert to the user.
+
+
+
+//CONSTANTS:
