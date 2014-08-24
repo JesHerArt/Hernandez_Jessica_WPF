fcd209767d438527250a67b6d6e64ddbbfecce21
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-24 12:00:13 -0400

    Functions worksheet (stung): The victimWeight variable has been declared in the givens section. The variable will be defined when the user inputs the victims weight in the prompt.
    A validation while loop is used to check if the user didn't input a value (is an empty string) or if it is less than zero.
    
    The input is then parsed as a number.

--- Functions_worksheet/Stung/JS/script.js
+++ Functions_worksheet/Stung/JS/script.js
@@ -9,3 +9,16 @@
 var overview = "BEE STINGS\n\nIt takes 8.6666666667 bee stings per pound to kill an animal. This program will calculate how many bee stings it will take to kill an animal depending on its weight.";
 
 alert(overview);
+
+
+
+//GIVENS:
+var victimWeight = prompt( "How many pounds(lbs.) does the animal victim weigh?" );
+
+while ( victimWeight == "" || victimWeight < 0 ){
+
+    victimWeight = prompt( "You've input an invalid weight in pounds for the animal victim's weight, please try again.\n\nHow many pounds(lbs.) does the animal victim weigh?" );
+
+}
+
+victimWeight = Number(victimWeight);
