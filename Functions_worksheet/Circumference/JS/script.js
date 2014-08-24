ca5666009e3bc0823da6ebbf6b63891db80dae43
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-23 22:25:59 -0400

    Functions worksheet (circumference): All comments for each line of code has been added. The validation check is no longer an if statement; I switched it to a while loop instead for continuous validation instead of just validating the input one time. The program fully works. The correct result is displayed each time the program is ran. Circumference problem complete.
    NOTE: The previous 7 commits about the functions worksheet assignment have been for the circumference problem as well.

--- Functions_worksheet/Circumference/JS/script.js
+++ Functions_worksheet/Circumference/JS/script.js
@@ -6,39 +6,44 @@
  */
 
 //OVERVIEW:
-var overview = "CIRCUMFERENCE CALCULATOR\n\nWhen you provide the radius of a circle, this program will calculate the circumference of the circle for you.";
+var overview = "CIRCUMFERENCE CALCULATOR\n\nWhen you provide the radius of a circle, this program will calculate the circumference of the circle for you.";    //Message to display to user about the overview of this calculation.
 
-alert(overview);
+alert(overview);  // Display the overview message as an alert to the user.
 
 
 
 //GIVENS:
-var radius = prompt( "What is the radius of the circle?" );
+var radius = prompt( "What is the radius of the circle?" );  //Prompts the user to enter a value for the circle radius. Declaring the radius variable and will get defined as the radius input the user enters.
 
-if ( radius == "" || radius < 0 ){
-    radius = prompt( "You've input an invalid length for the radius, please try again.\n\nWhat is the radius of the circle?" );
+while ( radius == "" || radius < 0 ){  //Validation check to ensure the user did not input an empty string or a value below zero.
+
+    radius = prompt( "You've input an invalid length for the radius, please try again.\n\nWhat is the radius of the circle?" );  //If the input is invalid, this prompt will let the user know an invalid input has been collected and ask them to input the radius value again.
+
 }
 
-radius = Number(radius);
+radius = Number(radius);  //Parsing the variable to a number.
 
 
 
 //FUNCTION
-function getCircumference( r ){
-    var circle = 2 * Math.PI * r;
-    return Math.round ( circle * 100 ) / 100;
+function getCircumference( r ){  //Function to get the circumference of a circle. The parameter will be defined as the radius of the circle when this function gets invoked.
+
+    var circle = 2 * Math.PI * r;  //The calculation for the circumference of a circle.
+
+    return Math.round ( circle * 100 ) / 100;  //Return the variable that holds the result of the circumference. This value will also get rounded to two decimal places.
+
 }
 
 
 
 //INVOKE THE FUNCTION
-var circumference = getCircumference(radius);
+var circumference = getCircumference(radius);  //The variable circumference calls the getCircumference function and passes the radius variable to then get defined as the returning result of the function.
 
 
 
 //RESULT TO PRINT
-var message = "CIRCUMFERENCE CALCULATOR\n\nA circle with the radius of " + radius + " has a circumference of " + circumference;
+var message = "CIRCUMFERENCE CALCULATOR\n\nA circle with the radius of " + radius + " has a circumference of " + circumference;  //Concatenating the resulting message to a string.
 
-console.log(message);
+console.log(message);   // Display the resulting message in the console.
 
-alert(message);
+alert(message);         // Display the resulting message as an alert to the user.
