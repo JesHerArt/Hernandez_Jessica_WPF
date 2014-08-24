34abe7babde45e49887807afa644e2c60fe9540b
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-24 11:49:26 -0400

    Functions worksheet (stung): An overview has been added to the beginning of the js file to alert the user what this program will calculate. The overview message will be displayed as an alert to the user.

--- Functions_worksheet/Stung/JS/script.js
+++ Functions_worksheet/Stung/JS/script.js
@@ -5,3 +5,7 @@
  Problem 2: Stung!
  */
 
+//OVERVIEW:
+var overview = "BEE STINGS\n\nIt takes 8.6666666667 bee stings per pound to kill an animal. This program will calculate how many bee stings it will take to kill an animal depending on its weight.";
+
+alert(overview);
