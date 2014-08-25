1cd89081228c4de5c29c14f38288d1abf7ce3591
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-25 18:01:55 -0400

    Functions Assignment (industry): An overview has been added to the beginning of the js file to let the user know what this program will calculate. The overview message will be displayed as an alert to the user.

--- Hernandez_Jessica_Functions/Functions_Industry/JS/script.js
+++ Hernandez_Jessica_Functions/Functions_Industry/JS/script.js
@@ -7,3 +7,9 @@
  */
 
 
+//OVERVIEW:
+var overview = "AVERAGE WAGE CALCULATOR\n\nBeing that your are a fairly new freelance Web Developer, after 3 months of freelancing you want to see if you are at least on the right track to making the estimated $67,540.00/year average as listed on the Bureau of Labor & Statistics government website. This program will calculate you average monthly income and compare to the estimated average to see if you're on the right track.";
+
+//The yearly average estimate is from: http://www.bls.gov/oes/current/oes151134.htm
+
+alert(overview);
