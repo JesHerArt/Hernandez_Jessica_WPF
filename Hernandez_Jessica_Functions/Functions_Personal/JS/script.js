005244c2d459570d8a52c9e330a41ecc54445f6b
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-25 15:52:51 -0400

    Functions Assignment (personal): The given section has been added. The five variables (gallons, gasPrice1, washPrice1, gasPrice2, washPrice2) have been declared and will be defined based on the users input at the prompts. Validation checks have all been made for all of the inputs the user gives for these variables. They check for empty strings of if the numbers are less then zero.

--- Hernandez_Jessica_Functions/Functions_Personal/JS/script.js
+++ Hernandez_Jessica_Functions/Functions_Personal/JS/script.js
@@ -12,3 +12,53 @@ alert(overview);
 
 
 //GIVENS:
+var gallons = prompt( "How many gallons does it take to completely fill up the gas tank in your car?" );
+
+while( gallons == "" || gallons < 0 ){
+
+    gallons = prompt( "An invalid amount of gallons has been entered, please try again.\n\n\nHow many gallons does it take to completely fill up the gas tank in your car?" );
+
+}
+
+gallons = Number(gallons);
+
+var gasPrice1 = prompt( "How much is Gas Station #1 charging per each gallon of gas?\n\n** Do not input the '$' symbol, just the price. **" );
+
+while ( gasPrice1 == "" || gasPrice1 < 0 ){
+
+    gasPrice1 = prompt( "An invalid amount has been entered for the gas price, please try again.\n\n\nHow much is Gas Station #1 charging per each gallon of gas?\n\n** Do not input the '$' symbol, just the price. **" );
+
+}
+
+gasPrice1 = Number(gasPrice1);
+
+var washPrice1 = prompt( "How much does a car wash cost at Gas Station #1\n\n** Do not input the '$' symbol, just the price. **" );
+
+while ( washPrice1 == "" || washPrice1 < 0 ){
+
+    washPrice1 = prompt( "An invalid amount has been entered for the car wash price, please try again.\n\n\nHow much does a car wash cost at Gas Station #1\n\n** Do not input the '$' symbol, just the price. **" );
+
+}
+
+washPrice1 = Number(washPrice1);
+
+
+var gasPrice2 = prompt( "How much is Gas Station #2 charging per each gallon of gas?\n\n** Do not input the '$' symbol, just the price. **" );
+
+while ( gasPrice2 == "" || gasPrice2 < 0 ){
+
+    gasPrice2 = prompt( "An invalid amount has been entered for the gas price, please try again.\n\n\nHow much is Gas Station #2 charging per each gallon of gas?\n\n** Do not input the '$' symbol, just the price. **" );
+
+}
+
+gasPrice2 = Number(gasPrice2);
+
+var washPrice2 = prompt( "How much does a car wash cost at Gas Station #2\n\n** Do not input the '$' symbol, just the price. **" );
+
+while ( washPrice2 == "" || washPrice2 < 0 ){
+
+    washPrice2 = prompt( "An invalid amount has been entered for the car wash price, please try again.\n\n\nHow much does a car wash cost at Gas Station #2\n\n** Do not input the '$' symbol, just the price. **" );
+
+}
+
+washPrice2 = Number(washPrice2);
