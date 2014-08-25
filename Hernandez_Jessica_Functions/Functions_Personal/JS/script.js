cdc1a963ed4e1fd51d0319fd93a369548e90d190
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-25 15:39:15 -0400

    Functions Assignment (personal): And overview has been added to the beginning of the js file to let the user know what this program will calculate. The overview message will be displayed as an alert to the user.

--- Hernandez_Jessica_Functions/Functions_Personal/JS/script.js
+++ Hernandez_Jessica_Functions/Functions_Personal/JS/script.js
@@ -4,3 +4,11 @@
  Functions Assignment: Personal
  */
 
+//OVERVIEW:
+var overview = "GAS STATION PRICE COMPARISON CALCULATOR\n\nYou need to go to the gas station to fill up your gas tank and also get a car wash.\nYou want to see which of the two gas stations will be cheaper to go to if you fully fill up your gas tank and get a car wash in the same gas station.";
+
+alert(overview);
+
+
+
+//GIVENS:
