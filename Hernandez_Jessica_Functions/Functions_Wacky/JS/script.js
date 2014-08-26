58c70cd275a74ea7208a0f4f064fc65ae735547f
Jessica Hernandez <jesherart@fullsail.edu>
2014-08-25 21:31:01 -0400

    Functions Assignment (wacky): The given section has been added. The numbers variable has been declared as an array to store the numbers the user will input. The for loop iterated through the array to store each number the user inputs.

--- Hernandez_Jessica_Functions/Functions_Wacky/JS/script.js
+++ Hernandez_Jessica_Functions/Functions_Wacky/JS/script.js
@@ -11,3 +11,26 @@
 var overview = "MEAN, MEDIAN & STANDARD DEVIATION CALCULATOR\n\nThis program will calculate the mean, median and standard deviation of five numbers you input.";
 
 alert(overview);
+
+
+
+//GIVENS:
+var numbers = new Array(5);
+
+var numCount = 1;
+
+for ( i = 0; i < numbers.length; i++ ){
+
+    numbers[i] = prompt("Number " + numCount + " out of 5\n\nEnter a number from 1 through 100.");
+
+    while ( numbers[i] == "" || numbers[i] < 1 || numbers[i] > 100 ){
+
+        numbers[i] = prompt( "An invalid number has been input, please try again.\n\n\nNumber " + numCount + " out of 5\n\nEnter a number from 1 through 100." );
+
+    }
+
+    numbers[i] = Number(numbers[i]);
+
+    numCount++;
+
+}
